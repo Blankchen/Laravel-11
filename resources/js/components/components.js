01e6f9edc6dcde04981751d6ff0670a1a83d7ef4
blank.chen <blank.chen@kiwigame.com>
2024-08-14 17:28:26 +0800

    test form ok

--- resources/js/components/components.js
+++ resources/js/components/components.js
@@ -1,4 +1,6 @@
 // ref: https://zerotomastery.io/blog/how-to-auto-register-components-for-vue-with-vite/
+import _ from 'lodash';
+
 const debug = true
 
 const importComponents = import.meta.glob("../**/*.vue",{ eager: true });
@@ -6,10 +8,9 @@ export const registerComponents = async (app) => {
     debug && console.log('importComponents: ', importComponents)
 
     Object.entries(importComponents).forEach(([fileName, componentConfig]) => {
-        const componentName = fileName
-        .split("/")
-        .pop()
-        ?.replace(/\.\w+$/, "");
+        const componentName = _.upperFirst(
+            _.camelCase(fileName.split('/').pop().replace(/\.\w+$/, ''))
+        );
 
     debug && console.log('registerComponents: ', componentName)
 
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,57 +1,5 @@
 @extends('layout.master')
 @section('content')
-<div>
-    123123123
-    <increment-counter />
-    {{-- <el-row>
-        <el-col :span="24">
-            @{{ date }}
-            <pp-date-picker v-model="date"></pp-date-picker>
-        </el-col>
-        <el-col :span="24">
-            @{{ select }}
-            <pp-select v-model="select" :list="[1,2,3,4,5,6]"></pp-select>
-        </el-col>
-        <el-col :span="24">
-            <el-pagination
-                v-model:current-page="pagination.page"
-                v-model:page-size="pagination.size"
-                :page-sizes="pagination.sizeList"
-                :total="pagination.total"
-                layout="sizes, total, prev, pager, next, jumper"
-            />
-        </el-col>
-    </el-row>
-    <el-button>@{{ message }}</el-button> --}}
-</div>
-{{--
-<script>
-    const Page = {
-        data() {
-        return {
-            date: '',
-            select: '',
-            message: "Hello Element Plus",
-            pagination: {
-                page: 1,
-                total: 0,
-                size: 50,
-                sizeList: [25, 50, 75, 100, 200]
-            },
-        };
-        },
-        methods: {
-            toggleCollapse() {
-                this.isCollapse = !this.isCollapse
-            }
-        }
-    };
-    const page = Vue.createApp(Page);
-    page.use(ElementPlus);
-    page.use(ppPlugin);
-    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-        page.component(key, component)
-    }
-    page.mount("#main_page");
-</script> --}}
+{{-- kebabCase --}}
+<table-form />
 @endsection
